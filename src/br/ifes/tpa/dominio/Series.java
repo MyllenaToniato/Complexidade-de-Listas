c32629a89bf37501d6e2d1a9bf5d69d7d4edb4af
Myllena <myllena.f.toniato@gmail.com>
2026-03-18 09:50:45 -0300

    Menu da classe Main

--- src/br/ifes/tpa/dominio/Series.java
+++ src/br/ifes/tpa/dominio/Series.java
@@ -1,1 +1,51 @@
+package br.ifes.tpa.dominio;
 
+public class Series {
+    private String nome; // Nome da série
+    private int ano; // Ano de lançamento
+    private String pais; // País de origem
+
+    public Series (String nome, int ano, String pais) {
+        this.nome=nome;
+        this.ano=ano;
+        this.pais=pais;
+    }
+
+    @Override
+    public boolean equals(Object a){
+        if (a instanceof Series)
+            return this.nome.equals(((Series) a).nome);
+        else
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " , " + Integer.toString(ano) + " , " + pais ;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+}
